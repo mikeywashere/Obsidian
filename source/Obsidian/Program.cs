932267bdd9cf355b322354a5275ea4189295e277
Michael R. Schmidt <mikenging@hotmail.com>
2025-09-30 14:00:35 -0700

    Add Bedrock downloader logic and update solution structure
    Updated Program.cs to implement Bedrock version fetching and UDP proxy startup using dependency injection and HttpClient. The solution file was updated for Visual Studio 18, added a 'tests' solution folder, and adjusted project nesting.

--- source/Obsidian/Program.cs
+++ source/Obsidian/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.DependencyInjection;
+using Obsidian;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Bedrock downloader");
+
+var sc = new ServiceCollection();
+sc.AddHttpClient();
+sc.AddTransient<IBedrock, Bedrock>();
+var sp = sc.BuildServiceProvider();
+var factory = sp.GetRequiredService<IHttpClientFactory>();
+
+var bedrock = new Bedrock(factory, RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? OSPlatform.Linux : OSPlatform.Windows);
+var v = await bedrock.Versions();
+Console.WriteLine($"Found {v.Count()} versions.");
+Console.WriteLine(
+    $"{string.Join(
+        $"{Environment.NewLine}",
+        v.Select(a => $"{a.Version}{(a.Preview ? " preview" : "")}")
+        .ToList())}");
+UdpProxy proxy = new UdpProxy(19132, "192.168.0.29", 19132);
+await proxy.StartAsync();
+
+Console.ReadLine();
